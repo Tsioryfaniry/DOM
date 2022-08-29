359946e21a3b0ac3565e4a66fe7ea1024d6c5f41
Tsioryfaniry <andrianarivonyfaniry@gmail.com>
2022-08-29 17:37:21 +0300

    Manipulation dom

---
+++ devoir.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector('.btn');
+let content = document.querySelector('.container');
+btn.addEventListener('click', function() {
+    content.classList.toggle('active');
+});
