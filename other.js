3567f1e7c18531667a911b05ae1a15a1ffeb3b06
Tsioryfaniry <andrianarivonyfaniry@gmail.com>
2022-08-30 10:56:18 +0300

    change bg color if click

--- other.js
+++ other.js
@@ -1,8 +1,12 @@
 function hello() {
+
+
     if (document.querySelector('h1').innerHTML === 'Hello!') {
+        document.querySelector('h1').style.backgroundColor = "red";
         document.querySelector('h1').innerHTML = 'Goodbye';
     } else {
         document.querySelector('h1').innerHTML = 'Hello!';
+        document.querySelector('h1').style.backgroundColor = "blue";
     }
 
 }
