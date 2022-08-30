4c305562ac17abd0c37657bf487e99be42d5bcdc
Tsioryfaniry <andrianarivonyfaniry@gmail.com>
2022-08-30 16:08:12 +0300

    added files feat/animation

--- animation.js
+++ animation.js
@@ -1,4 +1,24 @@
-function anim() {
-    document.querySelector('h1').style.color = 'red';
+/* function anim() {
+       document.querySelector('h1').style.color = 'red';
+      document.querySelector('.container')
+
 }
-document.querySelector('button').onclick = anim;
+document.querySelector('button').onclick = anim; */
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
