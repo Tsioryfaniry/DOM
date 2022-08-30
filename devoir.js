aa3023e1ea9af183d424229542c3f534e6b65fa0
Tsioryfaniry <andrianarivonyfaniry@gmail.com>
2022-08-30 10:43:26 +0300

    change devoir and style

--- devoir.js
+++ devoir.js
@@ -1,5 +1,5 @@
-let btn = document.querySelector('.btn');
+/* let btn = document.querySelector('.btn');
 let content = document.querySelector('.container');
 btn.addEventListener('click', function() {
     content.classList.toggle('active');
-});
+}); */
