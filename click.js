e6fade318a3b6034ce3e24fc4d41c2d10fb8d4e8
Tsioryfaniry <andrianarivonyfaniry@gmail.com>
2022-08-31 09:49:27 +0300

    change homme to femme and femme to homme

--- click.js
+++ click.js
@@ -1,10 +1,29 @@
 function change() {
-    document.querySelector('p').innerHTML = 'Article pour homme';
-    if (document.querySelector('p').innerHTML === 'Article pour homme') {
-        document.querySelector('p').innerHTML = 'Article pour femme';
 
-    } else if (document.querySelector('p').innerHTML === 'Article pour femme') {
-        document.querySelector('p').innerHTML = 'Article pour homme';
+    let paraSelec = document.querySelector('p');
+    let buttonSelec = document.querySelector('button');
+    let title = document.querySelector('h1');
+    let bg = document.querySelector('section');
+    let image = document.querySelector('img');
+    image.style.width = '150px';
+
+
+    if (paraSelec.innerHTML === 'Article pour homme', title.innerHTML === 'Homme', buttonSelec.innerHTML === 'change femme') {
+        paraSelec.innerHTML = 'Article pour femme';
+        title.innerHTML = 'Femme';
+        buttonSelec.innerHTML = 'change homme';
+        bg.style.backgroundColor = 'pink';
+        bg.style.color = 'black';
+        image.src = "temoignages.jpeg";
+    } else {
+        paraSelec.innerHTML = 'Article pour homme';
+        document.querySelector('h1').innerHTML = 'Homme';
+        buttonSelec.innerHTML = 'change femme';
+        bg.style.backgroundColor = 'blue';
+        bg.style.color = 'white';
+        image.src = "images.jpeg";
+
     }
 }
+
 document.querySelector('button').onclick = change;
